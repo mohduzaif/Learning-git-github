10be04f01ed4c0d817bd2cda6d193789ad06cae2
Mohd Uzaif <mohduzaif192@gmail.com>
2025-07-06 15:28:08 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# here we are writing some code of streamlit to learn git. 
+
+import streamlit as st
+
+
+st.title('CampusX')
+
+col1, col2 = st.columns(2)
+with col1: 
+    st.image('unnamed.png')
+with col2: 
+    st.write("""
+                A Machine Learning course can equip you with valuable skills for the future. It covers foundational concepts like supervised, unsupervised, and reinforcement learning, along with practical applications in areas like image/speech recognition and autonomous vehicles. Expect to learn about data analysis, model building, and evaluation techniques using tools like Python and relevant libraries.
+                Introduction to Machine Learning: Understanding the fundamentals and types of ML (supervised, unsupervised, reinforcement learning). 
+                Data Preprocessing: Cleaning, transforming, and preparing data for ML algorithms. 
+
+                """)
